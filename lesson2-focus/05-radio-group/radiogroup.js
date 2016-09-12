d3e651b117fb5f12d56e65e624827551b638327a
Vicky Gray <vicky.gray@digital.justice.gov.uk>
2016-09-12 16:36:12 +0100

    Implement keyboard event listeners

--- lesson2-focus/05-radio-group/radiogroup.js
+++ lesson2-focus/05-radio-group/radiogroup.js
@@ -32,7 +32,11 @@
         e.preventDefault();
 
         // This seems like a good place to do some stuff :)
-
+        if (this.focusedIdx === 0) {
+          this.focusedIdx = this.buttons.length - 1;
+        } else {
+          this.focusedIdx--;
+        }
         break;
 
       }
@@ -43,7 +47,11 @@
         e.preventDefault();
 
         // This seems like a good place to do some stuff :)
-
+        if (this.focusedIdx === this.buttons.length - 1) {
+          this.focusedIdx = 0;
+        } else {
+          this.focusedIdx++;
+        }
         break;
       }
 
